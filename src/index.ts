8c479e480ac34bc42dee12de11d2c0e0a2d7e1d6
bardak <hello@bardak.io>
2024-12-02 17:42:52 +0000

    .

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
 import type {
   Adapter,
-  PluginOptions as CloudStoragePluginOptions,
   CollectionOptions,
   GeneratedAdapter,
-} from '@payloadcms/plugin-cloud-storage/types'
-import type { Config, Plugin, UploadCollectionSlug } from 'payload'
+  PluginOptions as CloudStoragePluginOptions
+} from '@payloadcms/plugin-cloud-storage/types';
+import type {Config, Plugin, UploadCollectionSlug} from 'payload';
 
-import * as AWS from '@aws-sdk/client-s3'
-import { cloudStoragePlugin } from '@payloadcms/plugin-cloud-storage'
+import * as AWS from '@aws-sdk/client-s3';
+import {cloudStoragePlugin} from '@payloadcms/plugin-cloud-storage';
 
-import { getGenerateURL } from './generateURL.js'
-import { getHandleDelete } from './handleDelete.js'
-import { getHandleUpload } from './handleUpload.js'
-import { getHandler } from './staticHandler.js'
+import {getGenerateURL} from './generateURL.js';
+import {getHandleDelete} from './handleDelete.js';
+import {getHandleUpload} from './handleUpload.js';
+import {getHandler} from './staticHandler.js';
 
 export type S3StorageOptions = {
   /**
@@ -31,7 +31,7 @@ export type S3StorageOptions = {
   /**
    * Collection options to apply the S3 adapter to.
    */
-  collections: Partial<Record<UploadCollectionSlug, Omit<CollectionOptions, 'adapter'> | true>>
+  collections: Partial<Record<UploadCollectionSlug, Omit<CollectionOptions& { bucket?: string }, 'adapter'> | true>>
   /**
    * AWS S3 client configuration. Highly dependent on your AWS setup.
    *
@@ -58,73 +58,73 @@ type S3StoragePlugin = (storageS3Args: S3StorageOptions) => Plugin
 
 export const s3Storage: S3StoragePlugin =
   (s3StorageOptions: S3StorageOptions) =>
-  (incomingConfig: Config): Config => {
-    if (s3StorageOptions.enabled === false) {
-      return incomingConfig
-    }
-
-    const adapter = s3StorageInternal(s3StorageOptions)
-
-    // Add adapter to each collection option object
-    const collectionsWithAdapter: CloudStoragePluginOptions['collections'] = Object.entries(
-      s3StorageOptions.collections,
-    ).reduce(
-      (acc, [slug, collOptions]) => ({
-        ...acc,
-        [slug]: {
-          ...(collOptions === true ? {} : collOptions),
-          adapter,
-        },
-      }),
-      {} as Record<string, CollectionOptions>,
-    )
-
-    // Set disableLocalStorage: true for collections specified in the plugin options
-    const config = {
-      ...incomingConfig,
-      collections: (incomingConfig.collections || []).map((collection) => {
-        if (!collectionsWithAdapter[collection.slug]) {
-          return collection
-        }
-
-        return {
-          ...collection,
-          upload: {
-            ...(typeof collection.upload === 'object' ? collection.upload : {}),
-            disableLocalStorage: true,
-          },
-        }
-      }),
-    }
-
-    return cloudStoragePlugin({
-      collections: collectionsWithAdapter,
-    })(config)
-  }
-
-function s3StorageInternal({ acl, bucket, config = {} }: S3StorageOptions): Adapter {
-  return ({ collection, prefix }): GeneratedAdapter => {
-    let storageClient: AWS.S3 | null = null
-    const getStorageClient: () => AWS.S3 = () => {
-      if (storageClient) {
-        return storageClient
+    (incomingConfig: Config): Config => {
+      if(s3StorageOptions.enabled === false) {
+        return incomingConfig;
       }
-      storageClient = new AWS.S3(config)
-      return storageClient
-    }
+
+      // Add adapter to each collection option object
+      const collectionsWithAdapter: CloudStoragePluginOptions['collections'] = Object.entries(
+        s3StorageOptions.collections
+      ).reduce(
+        (acc, [slug, collOptions]) => ({
+          ...acc,
+          [slug]: {
+            ...(collOptions === true ? {} : collOptions),
+            adapter: s3StorageInternal({
+              ...s3StorageOptions, bucket: (collOptions === true ? {} : collOptions)?.bucket || s3StorageOptions.bucket
+            })
+          }
+        }),
+        {} as Record<string, CollectionOptions>
+      );
+
+      // Set disableLocalStorage: true for collections specified in the plugin options
+      const config = {
+        ...incomingConfig,
+        collections: (incomingConfig.collections || []).map((collection) => {
+          if(!collectionsWithAdapter[collection.slug]) {
+            return collection;
+          }
+
+          return {
+            ...collection,
+            upload: {
+              ...(typeof collection.upload === 'object' ? collection.upload : {}),
+              disableLocalStorage: true
+            }
+          };
+        })
+      };
+
+      return cloudStoragePlugin({
+        collections: collectionsWithAdapter
+      })(config);
+    };
+
+function s3StorageInternal({acl, bucket, config = {}}: S3StorageOptions): Adapter {
+  return ({collection, prefix}): GeneratedAdapter => {
+    let storageClient: AWS.S3 | null = null;
+    const getStorageClient: () => AWS.S3 = () => {
+      if(storageClient) {
+        return storageClient;
+      }
+      storageClient = new AWS.S3(config);
+      return storageClient;
+    };
 
     return {
       name: 's3',
-      generateURL: getGenerateURL({ bucket, config }),
-      handleDelete: getHandleDelete({ bucket, getStorageClient }),
+      generateURL: getGenerateURL({bucket, config}),
+      handleDelete: getHandleDelete({bucket, getStorageClient}),
       handleUpload: getHandleUpload({
         acl,
         bucket,
         collection,
         getStorageClient,
-        prefix,
+        prefix
       }),
-      staticHandler: getHandler({ bucket, collection, getStorageClient }),
-    }
-  }
+      staticHandler: getHandler({bucket, collection, getStorageClient})
+    };
+  };
 }
